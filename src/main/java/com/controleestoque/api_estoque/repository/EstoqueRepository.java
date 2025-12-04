89829b4091ceeed74759423feded8e93fb5ae26e
Murilo Duarte Carpinedo <carpinedo.muri@gmail.com>
2025-12-04 17:11:23 -0300

    repository

---
+++ src/main/java/com/controleestoque/api_estoque/repository/EstoqueRepository.java
@@ -0,0 +1,11 @@
+package com.controleestoque.api_estoque.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import com.controleestoque.api_estoque.model.Estoque;
+import java.util.Optional;
+
+@Repository
+public interface EstoqueRepository extends JpaRepository<Estoque, Long> {
+    Optional<Estoque> findByProduto_Id(Long produtoId);
+}
